8ec9d410e229d3d4b99d8291e672f69e4de568f2
Thibaud Colas <thibaudcolas@gmail.com>
2016-11-13 20:23:51 +0200

    Add configurable block types and inline styles

--- examples/basic.js
+++ examples/basic.js
@@ -12,10 +12,40 @@ const options = {
     dialogControls: [],
     sources: [],
     decorators: [],
+    BLOCK_TYPES: [
+        { label: 'H2', style: 'header-two' },
+        { label: 'H3', style: 'header-three' },
+        { label: 'UL', style: 'unordered-list-item', icon: 'list-ul' },
+    ],
+
+    INLINE_STYLES: [
+        { label: 'Bold', style: 'BOLD', icon: 'bold' },
+        { label: 'Italic', style: 'ITALIC', icon: 'italic' },
+    ],
+};
+
+const value = {
+    entityMap: {},
+    blocks: [
+        {
+            key: '6mgfh',
+            text: 'User experience (UX) design',
+            type: 'header-two',
+            depth: 0,
+            inlineStyleRanges: [
+                {
+                    offset: 16,
+                    length: 4,
+                    style: 'ITALIC',
+                },
+            ],
+            entityRanges: [],
+        },
+    ],
 };
 
 const editor = (
-    <DraftailEditor name="basic" value="{}" options={options} />
+    <DraftailEditor name="basic" value={JSON.stringify(value)} options={options} />
 );
 
 ReactDOM.render(editor, mount);
--- examples/entities.js
+++ examples/entities.js
@@ -69,6 +69,23 @@ const options = {
         // { strategy: findModelEntities, component: Model },
         // { strategy: findDocumentEntities, component: Document },
     ],
+    BLOCK_TYPES: [
+        { label: 'H2', style: 'header-two' },
+        { label: 'H3', style: 'header-three' },
+        { label: 'H4', style: 'header-four' },
+        { label: 'H5', style: 'header-five' },
+        { label: 'Blockquote', style: 'blockquote', icon: 'openquote' },
+        { label: 'UL', style: 'unordered-list-item', icon: 'list-ul' },
+        { label: 'OL', style: 'ordered-list-item', icon: 'list-ol' },
+    ],
+
+    INLINE_STYLES: [
+        { label: 'Bold', style: 'BOLD', icon: 'bold' },
+        { label: 'Italic', style: 'ITALIC', icon: 'italic' },
+        // {label: 'Underline', style: 'UNDERLINE'},
+        // {label: 'Monospace', style: 'CODE'},
+        // {label: 'Strikethrough', style: 'STRIKETHROUGH'},
+    ],
 };
 
 const editor = (
