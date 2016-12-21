b10811785bad9936cc24e6704269037f7bf9ac11
Thibaud Colas <thibaudcolas@gmail.com>
2016-12-21 15:48:47 +1300

    Linting

--- examples/entities.js
+++ examples/entities.js
@@ -15,6 +15,8 @@ import Link, { findLinkEntities } from './entities/Link';
 const mount = document.querySelector('[data-mount-entities]');
 
 const options = {
+    enableHorizontalRule: true,
+    enableLineBreak: true,
     modelPickerOptions: [],
     MODEL: ENTITY_TYPE.MODEL,
     imageFormats: [],
