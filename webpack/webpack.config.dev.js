a14f91ec1db61ec487add21ca8c04d8e843b8cfa
Thibaud Colas <thibaudcolas@gmail.com>
2017-05-25 19:55:52 +0300

    Move polyfills outside of main lib

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -14,7 +14,7 @@ module.exports = {
     // See http://webpack.github.io/docs/configuration.html#devtool
     devtool: 'inline-source-map',
     entry: {
-        vendor: ['react', 'react-dom', 'immutable', 'draft-js', 'draftjs-utils'],
+        vendor: ['react', 'react-dom', 'immutable', 'draft-js', 'draftjs-utils', './examples/polyfills'],
         intro: './examples/intro',
         basic: './examples/basic',
         entities: './examples/entities',
