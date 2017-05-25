4e6c944832f1d6b9613e50abbb40ea8ea99cb57c
Thibaud Colas <thibaudcolas@gmail.com>
2017-05-25 18:58:29 +0300

    Replace custom server code with webpack-dev-server, updating to Webpack 2

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,17 +2,14 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
 const config = require('./webpack.config.dev');
 
-// Lose the webpack middleware
-delete config.entry.hmr;
 config.watch = false;
 config.devtool = false;
 config.output.path = path.join(__dirname, 'pages', 'assets');
 
 config.plugins = [
-    new ProgressBarPlugin(),
     new webpack.DefinePlugin({
         // Key is hard-coded because it will be public on the demo site anyway.
         // Key usage is limited to whitelisted Referrers.
@@ -21,7 +18,7 @@ config.plugins = [
             NODE_ENV: JSON.stringify('production'),
         },
     }),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js', Infinity),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
     new webpack.optimize.UglifyJsPlugin({
         compress: {
             screw_ie8: true, // React doesn't support IE8
