5148051bb0383e19790c32560b73d2a61e02569f
Thibaud Colas <thibaudcolas@gmail.com>
2017-05-29 23:33:27 +0300

    Remove vendored polyfills in favor of element-closest package

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -35,7 +35,14 @@ module.exports = {
     // See http://webpack.github.io/docs/configuration.html#devtool
     devtool: 'inline-source-map',
     entry: {
-        vendor: ['react', 'react-dom', 'immutable', 'draft-js', 'draftjs-utils', './examples/utils/polyfills'],
+        vendor: [
+            'react',
+            'react-dom',
+            'immutable',
+            'draft-js',
+            'draftjs-utils',
+            'element-closest',
+        ],
         intro: './examples/intro',
         basic: './examples/basic',
         entities: './examples/entities',
