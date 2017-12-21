649895c3fa100d163ed5bfd7949116215d9e387d
Thibaud Colas <thibaudcolas@gmail.com>
2017-12-21 22:06:36 +0200

    Add missing ImageBlock tests

--- examples/blocks/ImageBlock.test.js
+++ examples/blocks/ImageBlock.test.js
@@ -1,119 +1,73 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { convertFromRaw, EditorState } from 'draft-js';
 
-import { ENTITY_TYPE } from '../../lib';
+import { DraftUtils } from '../../lib';
 
 import ImageBlock from '../blocks/ImageBlock';
 
-const contentState = convertFromRaw({
-    entityMap: {
-        '0': {
-            type: ENTITY_TYPE.IMAGE,
-            mutability: 'IMMUTABLE',
-            data: {
-                src:
-                    'https://springload.github.io/draftail/static/example-lowres-image.jpg',
-            },
-        },
-    },
-    blocks: [
-        {
-            key: 'b3kdk',
-            text: ' ',
-            type: 'atomic',
-            depth: 0,
-            inlineStyleRanges: [],
-            entityRanges: [
-                {
-                    offset: 0,
-                    length: 1,
-                    key: 0,
-                },
-            ],
-            data: {},
-        },
-    ],
-});
-
-const mockProps = {
-    block: contentState.getFirstBlock(),
-    blockProps: {
-        editorState: EditorState.createWithContent(contentState),
-        entity: {
-            getType: () => ENTITY_TYPE.IMAGE,
-            getData: () => ({
-                src: 'http://www.example.com/example.png',
-                alt: 'Test alt',
-            }),
-        },
-        entityConfig: {
-            type: ENTITY_TYPE.IMAGE,
-        },
-        onChange: jest.fn(),
-        lockEditor: jest.fn(),
-        unlockEditor: jest.fn(),
-    },
-    contentState,
-};
-
 describe('ImageBlock', () => {
-    it('empty', () => {
+    it('renders', () => {
         expect(
             shallow(
                 <ImageBlock
-                    {...mockProps}
-                    entity={{
-                        getData() {
-                            return {};
+                    block={{}}
+                    blockProps={{
+                        editorState: {},
+                        entityConfig: {},
+                        entity: {
+                            getData: () => ({
+                                src: 'example.png',
+                            }),
                         },
+                        onChange: () => {},
                     }}
                 />,
             ),
         ).toMatchSnapshot();
     });
 
-    it('basic', () => {
-        expect(shallow(<ImageBlock {...mockProps} />)).toMatchSnapshot();
-    });
-
-    it('#entityConfig.icon', () => {
+    it('alt', () => {
         expect(
             shallow(
                 <ImageBlock
-                    {...mockProps}
-                    entityConfig={{
-                        icon: '#icon-image',
-                    }}
-                />,
-            ),
-        ).toMatchSnapshot();
-    });
-
-    it('#isActive', () => {
-        expect(
-            shallow(<ImageBlock {...mockProps} isActive={true} />),
-        ).toMatchSnapshot();
-    });
-
-    it('empty #isActive', () => {
-        expect(
-            shallow(
-                <ImageBlock
-                    {...mockProps}
-                    isActive={true}
-                    entity={{
-                        getData() {
-                            return {};
+                    block={{}}
+                    blockProps={{
+                        editorState: {},
+                        entityConfig: {},
+                        entity: {
+                            getData: () => ({
+                                src: 'example.png',
+                                alt: 'Test',
+                            }),
                         },
+                        onChange: () => {},
                     }}
                 />,
             ),
         ).toMatchSnapshot();
     });
 
-    it('changeText', () => {
-        const wrapper = shallow(<ImageBlock {...mockProps} isActive={true} />);
+    it('changeAlt', () => {
+        jest.spyOn(DraftUtils, 'updateBlockEntity');
+        DraftUtils.updateBlockEntity.mockImplementation(e => e);
+
+        const onChange = jest.fn();
+        const wrapper = shallow(
+            <ImageBlock
+                block={{}}
+                blockProps={{
+                    editorState: {},
+                    entityConfig: {},
+                    entity: {
+                        getData: () => ({
+                            src: 'example.png',
+                            alt: 'Test',
+                        }),
+                    },
+                    onChange,
+                }}
+            />,
+        );
 
         wrapper.find('[type="text"]').simulate('change', {
             currentTarget: {
@@ -121,8 +75,13 @@ describe('ImageBlock', () => {
             },
         });
 
-        expect(mockProps.onSave).toHaveBeenCalledWith(
+        expect(onChange).toHaveBeenCalled();
+        expect(DraftUtils.updateBlockEntity).toHaveBeenCalledWith(
+            expect.any(Object),
+            {},
             expect.objectContaining({ alt: 'new alt' }),
         );
+
+        jest.restoreAllMocks();
     });
 });
