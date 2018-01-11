6b5e58bd96f70b34480ff6c5130a4f9df540e96d
Thibaud Colas <thibaudcolas@gmail.com>
2018-01-11 19:24:43 +0200

    Switch to rollup for package compilation

---
+++ webpack/rollup.config.js
@@ -0,0 +1,24 @@
+import babel from 'rollup-plugin-babel';
+import pkg from '../package.json';
+
+export default [
+    {
+        input: 'lib/index.js',
+        external: ['draft-js/lib/isSoftNewlineEvent']
+            .concat(Object.keys(pkg.dependencies))
+            .concat(Object.keys(pkg.peerDependencies)),
+        output: [
+            { file: pkg.main, format: 'cjs' },
+            { file: pkg.module, format: 'es' },
+        ],
+        plugins: [
+            babel(
+                Object.assign({}, pkg.babel, {
+                    babelrc: false,
+                    plugins: ['external-helpers'],
+                    exclude: ['node_modules/**'],
+                }),
+            ),
+        ],
+    },
+];
