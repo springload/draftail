90cf148228831ce33a7fd78bf6425cd4868596c9
Thibaud Colas <thibaudcolas@gmail.com>
2018-02-14 18:42:37 +0200

    Add watch mode to integration tests

--- tests/integration/teardown.js
+++ tests/integration/teardown.js
@@ -4,10 +4,12 @@ const path = require('path');
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
-module.exports = async function() {
+module.exports = async () => {
     await global.BROWSER.close();
 
     rimraf.sync(DIR);
 
-    await global.SERVER.close();
+    if (global.SERVER) {
+        await global.SERVER.close();
+    }
 };
