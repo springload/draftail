0e0eb63d402f09b0b43c8f093b50add7f945b316
Thibaud Colas <thibaudcolas@gmail.com>
2018-02-14 18:52:56 +0200

    Remove timeout for integration tests beforeAll

--- tests/integration/index.test.js
+++ tests/integration/index.test.js
@@ -2,14 +2,12 @@ import { toHaveNoViolations } from 'jest-axe';
 
 expect.extend(toHaveNoViolations);
 
-const timeout = 5000;
-
 describe('/', () => {
     let page;
     beforeAll(async () => {
         page = await global.BROWSER.newPage();
         await page.goto(global.ROOT);
-    }, timeout);
+    });
 
     it('should load without error', async () => {
         const text = await page.evaluate(() => document.body.textContent);
