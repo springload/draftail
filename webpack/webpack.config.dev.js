04c692d0b78a6930118531873499ac16a22c7d7a
Thibaud Colas <thibaudcolas@gmail.com>
2018-09-29 02:33:43 +0300

    Move Webpack config into a single file

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -2,27 +2,4 @@ const base = require('./webpack.config.base');
 
 const config = base('development');
 
-module.exports = Object.assign({}, config, {
-    // See http://webpack.github.io/docs/configuration.html#devtool
-    devtool: 'inline-source-map',
-
-    // Turn off performance hints during development because we don't do any
-    // splitting or minification in interest of speed. These warnings become
-    // cumbersome.
-    performance: {
-        hints: false,
-    },
-
-    // https://webpack.js.org/configuration/dev-server/#devserver
-    devServer: {
-        contentBase: config.output.path,
-        watchContentBase: true,
-        compress: true,
-        hot: true,
-        port: 4000,
-        overlay: true,
-        clientLogLevel: 'none',
-        stats: config.stats,
-        disableHostCheck: true,
-    },
-});
+module.exports = config;
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -2,7 +2,4 @@ const base = require('./webpack.config.base');
 
 const config = base('production');
 
-module.exports = Object.assign({}, config, {
-    // See http://webpack.github.io/docs/configuration.html#devtool
-    devtool: 'source-map',
-});
+module.exports = config;
