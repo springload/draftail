097d2a86aaa63d6a874c761b0f1b05fed35e9779
Thibaud Colas <thibaudcolas@gmail.com>
2018-09-29 05:25:29 +0300

    Switch formatting tools to 2-space indent for all files

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,12 @@
 // See https://prettier.io/docs/en/options.html.
 module.exports = {
-    printWidth: 80,
-    tabWidth: 4,
-    useTabs: false,
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    arrowParens: 'always',
-    proseWrap: 'preserve',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+  proseWrap: "preserve",
 };
