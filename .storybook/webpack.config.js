ba5c22e8cb098e87c3b635457232dac33e14b70f
Thibaud Colas <thibaudcolas@gmail.com>
2018-10-25 10:09:47 +0300

    Add further Storybook Webpack configuration

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,32 @@
 const path = require("path");
 const webpack = require("webpack");
 const sass = require("sass");
+const autoprefixer = require("autoprefixer");
 
 const pkg = require("../package.json");
 
 module.exports = (baseConfig, env, defaultConfig) => {
+  // See http://webpack.github.io/docs/configuration.html#devtool
+  defaultConfig.devtool = "source-map";
+
   defaultConfig.module.rules.push({
     test: /\.(scss|css)$/,
     loaders: [
       "style-loader",
-      "css-loader",
+      {
+        loader: "css-loader",
+        options: {
+          sourceMap: true,
+          minimize: true,
+        },
+      },
+      {
+        loader: "postcss-loader",
+        options: {
+          sourceMap: true,
+          plugins: () => [autoprefixer()],
+        },
+      },
       {
         loader: "sass-loader",
         options: {
