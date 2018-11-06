6af610e130f7d4de14f356902915fb285e866541
Thibaud Colas <thibaudcolas@gmail.com>
2018-11-07 00:13:09 +0200

    Clean up Simple example

--- examples/simple.story.js
+++ examples/simple.story.js
@@ -3,19 +3,18 @@ import React from "react";
 
 import { DraftailEditor, BLOCK_TYPE, INLINE_STYLE } from "../lib";
 
-const initial = JSON.parse(sessionStorage.getItem("draftail:content"));
+const initial = JSON.parse(sessionStorage.getItem("content"));
 
 const onSave = (content) => {
   // eslint-disable-next-line no-console
   console.log("saving", content);
-  sessionStorage.setItem("draftail:content", JSON.stringify(content));
+  sessionStorage.setItem("content", JSON.stringify(content));
 };
 
 storiesOf("Draftail", module).add("Simple", () => (
   <DraftailEditor
     rawContentState={initial || null}
     onSave={onSave}
-    ariaDescribedBy="simple-editor"
     blockTypes={[
       { type: BLOCK_TYPE.HEADER_THREE },
       { type: BLOCK_TYPE.UNORDERED_LIST_ITEM },
