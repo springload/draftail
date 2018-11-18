766249c06f79b68b6ba17b5bf471ed4a033ae0ab
Thibaud Colas <thibaudcolas@gmail.com>
2018-11-19 00:13:12 +0200

    Fix image URL

--- examples/docs.story.js
+++ examples/docs.story.js
@@ -286,7 +286,7 @@ storiesOf("Docs", module)
     <p>This editor demonstrates <strong>HTML import and export</strong>.</p>
     <hr/>
     <blockquote>Built with <a href="https://github.com/HubSpot/draft-convert">draft-convert</a></blockquote>
-    <img src="static/example-lowres-image2.jpg"/>
+    <img src="/static/example-lowres-image2.jpg"/>
     <p></p>
     `;
 
