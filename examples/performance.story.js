24cb2795f9a3d9c8eb4ad6dfc6677f1088439972
Thibaud Colas <thibaudcolas@gmail.com>
2018-12-14 17:41:01 +0100

    Add performance monitoring in the performance testing stories

--- examples/performance.story.js
+++ examples/performance.story.js
@@ -5,8 +5,10 @@ import contentStates from "markov_draftjs";
 import { DraftailEditor } from "../lib";
 
 import { INLINE_CONTROL, BLOCK_CONTROL, ENTITY_CONTROL } from "./constants/ui";
+import StatsGraph from "./components/StatsGraph";
 
 const NB_EDITORS = 50;
+const NB_EDITORS_LOW = 5;
 const MAX_EDITORS = contentStates.length;
 
 const benchmarkProps = {
@@ -27,28 +29,79 @@ const benchmarkProps = {
   inlineStyles: [INLINE_CONTROL.BOLD, INLINE_CONTROL.ITALIC],
 };
 
+const header = (
+  <>
+    <p>
+      Enable the{" "}
+      <a
+        className="link"
+        href="https://developers.google.com/web/tools/chrome-devtools/evaluate-performance/reference#fps-meter"
+      >
+        Chrome DevTools FPS meter
+      </a>
+    </p>
+    <StatsGraph />
+  </>
+);
+
 storiesOf("Performance", module)
+  .add(`markov_draftjs 0-${NB_EDITORS_LOW}`, () => (
+    <>
+      {header}
+      <>
+        {contentStates.slice(0, NB_EDITORS_LOW).map((contentState, i) => (
+          <DraftailEditor
+            // eslint-disable-next-line @thibaudcolas/cookbook/react/no-array-index-key
+            key={i}
+            rawContentState={contentState}
+            {...benchmarkProps}
+          />
+        ))}
+      </>
+    </>
+  ))
   .add(`markov_draftjs 0-${NB_EDITORS}`, () => (
     <>
-      {contentStates.slice(0, NB_EDITORS).map((contentState, i) => (
-        <DraftailEditor
-          // eslint-disable-next-line @thibaudcolas/cookbook/react/no-array-index-key
-          key={i}
-          rawContentState={contentState}
-          {...benchmarkProps}
-        />
-      ))}
+      {header}
+      <>
+        {contentStates.slice(0, NB_EDITORS).map((contentState, i) => (
+          <DraftailEditor
+            // eslint-disable-next-line @thibaudcolas/cookbook/react/no-array-index-key
+            key={i}
+            rawContentState={contentState}
+            {...benchmarkProps}
+          />
+        ))}
+      </>
+    </>
+  ))
+  .add(`markov_draftjs ${MAX_EDITORS - NB_EDITORS_LOW}-${MAX_EDITORS}`, () => (
+    <>
+      {header}
+      <>
+        {contentStates.slice(-NB_EDITORS_LOW).map((contentState, i) => (
+          <DraftailEditor
+            // eslint-disable-next-line @thibaudcolas/cookbook/react/no-array-index-key
+            key={i}
+            rawContentState={contentState}
+            {...benchmarkProps}
+          />
+        ))}
+      </>
     </>
   ))
   .add(`markov_draftjs ${MAX_EDITORS - NB_EDITORS}-${MAX_EDITORS}`, () => (
     <>
-      {contentStates.slice(-NB_EDITORS).map((contentState, i) => (
-        <DraftailEditor
-          // eslint-disable-next-line @thibaudcolas/cookbook/react/no-array-index-key
-          key={i}
-          rawContentState={contentState}
-          {...benchmarkProps}
-        />
-      ))}
+      {header}
+      <>
+        {contentStates.slice(-NB_EDITORS).map((contentState, i) => (
+          <DraftailEditor
+            // eslint-disable-next-line @thibaudcolas/cookbook/react/no-array-index-key
+            key={i}
+            rawContentState={contentState}
+            {...benchmarkProps}
+          />
+        ))}
+      </>
     </>
   ));
