b9aca08c390aa4a616b9ea784f0736dd531a6caa
Thibaud Colas <thibaudcolas@gmail.com>
2018-12-15 13:32:28 +0100

    Use react-component-benchmark for all stories

--- examples/components/EditorWrapper.js
+++ examples/components/EditorWrapper.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import Benchmark, { BenchmarkType } from "react-component-benchmark";
 
 import { DraftailEditor } from "../../lib";
 
@@ -16,9 +17,12 @@ class EditorWrapper extends Component {
 
     this.state = {
       content: null,
+      benchmarkResults: null,
     };
 
     this.onSave = this.onSave.bind(this);
+    this.startBenchmark = this.startBenchmark.bind(this);
+    this.onBenchmarkComplete = this.onBenchmarkComplete.bind(this);
   }
 
   onSave(content) {
@@ -33,9 +37,21 @@ class EditorWrapper extends Component {
     }
   }
 
+  onBenchmarkComplete(results) {
+    this.setState({
+      benchmarkResults: results,
+    });
+  }
+
+  startBenchmark() {
+    if (this.benchmark) {
+      this.benchmark.start();
+    }
+  }
+
   render() {
     const { id } = this.props;
-    const { content } = this.state;
+    const { content, benchmarkResults } = this.state;
     const initialContent =
       JSON.parse(sessionStorage.getItem(`${id}:content`)) || null;
     return (
@@ -56,6 +72,48 @@ class EditorWrapper extends Component {
               <span>Version: {DRAFTAIL_VERSION}</span>
             </li>
           </ul>
+          <button type="button" onClick={this.startBenchmark}>
+            Benchmark
+          </button>
+          <Benchmark
+            component={DraftailEditor}
+            componentProps={this.props}
+            onComplete={this.onBenchmarkComplete}
+            ref={(ref) => {
+              this.benchmark = ref;
+            }}
+            samples={20}
+            timeout={10000}
+            type={BenchmarkType.MOUNT}
+          />
+          {benchmarkResults ? (
+            <table>
+              <thead>
+                <tr>
+                  <th>mean</th>
+                  <th>min</th>
+                  <th>median</th>
+                  <th>p70</th>
+                  <th>p95</th>
+                  <th>p99</th>
+                  <th>max</th>
+                  <th>stdDev</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{benchmarkResults.mean.toFixed(1)}</td>
+                  <td>{benchmarkResults.min.toFixed(1)}</td>
+                  <td>{benchmarkResults.median.toFixed(1)}</td>
+                  <td>{benchmarkResults.p70.toFixed(1)}</td>
+                  <td>{benchmarkResults.p95.toFixed(1)}</td>
+                  <td>{benchmarkResults.p99.toFixed(1)}</td>
+                  <td>{benchmarkResults.max.toFixed(1)}</td>
+                  <td>{benchmarkResults.stdDev.toFixed(1)}</td>
+                </tr>
+              </tbody>
+            </table>
+          ) : null}
           <Highlight
             language="js"
             value={JSON.stringify(content || initialContent, null, 2)}
