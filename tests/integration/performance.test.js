6f429bd1f1a670f5b1f00e25234b52d96863dded
Thibaud Colas <thibaudcolas@gmail.com>
2018-12-15 18:07:03 +0100

    Add integration-level performance tests

---
+++ tests/integration/performance.test.js
@@ -0,0 +1,51 @@
+describe("performance", () => {
+  let page;
+  beforeEach(async () => {
+    page = await global.BROWSER.newPage();
+  });
+
+  it("simple editor", async () => {
+    await page.goto(
+      `${global.ROOT}?selectedKind=Draftail&selectedStory=Simple`,
+    );
+
+    await page.type('[role="textbox"]', "Hello");
+    await page.keyboard.press("Enter");
+    await page.type('[role="textbox"]', "- ");
+    await page.type('[role="textbox"]', "World");
+    await page.keyboard.press("Enter");
+
+    const heapSize = await page.evaluate(
+      () => window.performance.memory.usedJSHeapSize,
+    );
+    const heapSizeMB = heapSize / 10 ** 6;
+    expect(heapSizeMB).toBeLessThanOrEqual(20.5);
+  });
+
+  it("markov_draftjs 41 memory", async () => {
+    await page.goto(
+      `${
+        global.ROOT
+      }?selectedKind=Performance&selectedStory=markov_draftjs%2041`,
+    );
+
+    const heapSize = await page.evaluate(
+      () => window.performance.memory.usedJSHeapSize,
+    );
+    const heapSizeMB = heapSize / 10 ** 6;
+    expect(heapSizeMB).toBeLessThanOrEqual(19);
+  });
+
+  it("markov_draftjs 41 speed", async () => {
+    await page.goto(
+      `${
+        global.ROOT
+      }?selectedKind=Performance&selectedStory=markov_draftjs%2041`,
+    );
+
+    const mean = await page.$eval('[data-benchmark="mean"]', (elt) =>
+      parseFloat(elt.innerHTML),
+    );
+    expect(mean).toBeLessThanOrEqual(42);
+  });
+});
