a247b6eed583cbf127257b73a42d4a0cb8019969
Thibaud Colas <thibaudcolas@gmail.com>
2018-12-16 18:21:54 +0100

    Exclude examples code from unit test coverage metrics for now

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "\\.scss$": "<rootDir>/tests/styleMock.js",
   },
   testPathIgnorePatterns: ["/node_modules/", "/dist/", "/es/", "/integration/"],
-  coveragePathIgnorePatterns: ["<rootDir>/tests"],
+  coveragePathIgnorePatterns: ["<rootDir>/tests", "/examples/"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
   setupFiles: ["<rootDir>/tests/polyfills.js"],
   setupTestFrameworkScriptFile: "<rootDir>/tests/setupTest.js",
