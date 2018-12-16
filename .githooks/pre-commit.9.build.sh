93454c65f6d1ba27bfbd80f842d60475f26a9155
Thibaud Colas <thibaudcolas@gmail.com>
2018-12-16 22:05:48 +0100

    Improve integration of performance troubleshooting tools with dev workflow

--- .githooks/pre-commit.9.build.sh
+++ .githooks/pre-commit.9.build.sh
@@ -4,4 +4,6 @@ if [ -n "$JS_STAGED" ] || [ -n "$SCSS_STAGED" ] || [ -n "$HTML_STAGED" ];
 then
     npm run dist -s
     npm run test:integration -s
+    npm run test:performance -s
+    npm run report:size -s
 fi
