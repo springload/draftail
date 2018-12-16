e1296f81253379d3be788cae0a92051a7c9fbb2a
Thibaud Colas <thibaudcolas@gmail.com>
2018-12-16 15:45:26 +0100

    Move Jest config to a separate file

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  moduleNameMapper: {
+    "\\.scss$": "<rootDir>/tests/styleMock.js",
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/es/", "/integration/"],
+  coveragePathIgnorePatterns: ["<rootDir>/tests"],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupFiles: ["<rootDir>/tests/polyfills.js"],
+  setupTestFrameworkScriptFile: "<rootDir>/tests/setupTest.js",
+};
