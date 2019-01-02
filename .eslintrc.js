29a7f5dbd00639e916081d780c47cc7d8a450074
Thibaud Colas <thibaudcolas@gmail.com>
2019-01-02 23:48:59 +0100

    Use ESLint plugin for Flow type definitions

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
   extends: [
     "plugin:@thibaudcolas/eslint-plugin-cookbook/recommended",
     "plugin:compat/recommended",
+    "plugin:flowtype/recommended",
   ],
+  plugins: ["flowtype"],
+  rules: {
+    "flowtype/space-after-type-colon": [0],
+  },
   settings: {
     polyfills: ["promises"],
   },
