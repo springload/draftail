d991d4d6d8ceada82aeb8c0a77fb66128946bc4b
Thibaud Colas <thibaudcolas@gmail.com>
2019-02-05 13:02:45 +0000

    Add example of a "linkify" plugin

--- examples/plugins.story.js
+++ examples/plugins.story.js
@@ -1,18 +1,31 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
 
-import { INLINE_STYLE } from "../lib";
+import { INLINE_CONTROL, ENTITY_CONTROL, BLOCK_CONTROL } from "./constants/ui";
 
 import EditorWrapper from "./components/EditorWrapper";
 import singleLinePlugin from "./plugins/singleLinePlugin";
+import linkifyPlugin from "./plugins/linkifyPlugin";
 
 const singleLine = singleLinePlugin();
+const linkify = linkifyPlugin();
 
-storiesOf("Plugins", module).add("Single-line", () => (
-  <EditorWrapper
-    id="single-line"
-    inlineStyles={[{ type: INLINE_STYLE.BOLD }, { type: INLINE_STYLE.ITALIC }]}
-    enableLineBreak
-    plugins={[singleLine]}
-  />
-));
+storiesOf("Plugins", module)
+  .add("Single-line", () => (
+    <EditorWrapper
+      id="single-line"
+      inlineStyles={[INLINE_CONTROL.BOLD, INLINE_CONTROL.ITALIC]}
+      entityTypes={[ENTITY_CONTROL.LINK]}
+      enableLineBreak
+      plugins={[singleLine]}
+    />
+  ))
+  .add("Linkify", () => (
+    <EditorWrapper
+      id="linkify"
+      inlineStyles={[INLINE_CONTROL.BOLD]}
+      blockTypes={[BLOCK_CONTROL.UNORDERED_LIST_ITEM]}
+      entityTypes={[ENTITY_CONTROL.LINK]}
+      plugins={[linkify]}
+    />
+  ));
