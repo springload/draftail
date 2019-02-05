591aa975d58ddbcbb5e3b44e8561dd7cf31d9aba
Thibaud Colas <thibaudcolas@gmail.com>
2019-02-05 13:10:18 +0000

    Add example plugin for action lists

--- examples/plugins.story.js
+++ examples/plugins.story.js
@@ -6,9 +6,11 @@ import { INLINE_CONTROL, ENTITY_CONTROL, BLOCK_CONTROL } from "./constants/ui";
 import EditorWrapper from "./components/EditorWrapper";
 import singleLinePlugin from "./plugins/singleLinePlugin";
 import linkifyPlugin from "./plugins/linkifyPlugin";
+import actionBlockPlugin from "./plugins/actionBlockPlugin";
 
 const singleLine = singleLinePlugin();
 const linkify = linkifyPlugin();
+const actionBlock = actionBlockPlugin();
 
 storiesOf("Plugins", module)
   .add("Single-line", () => (
@@ -27,5 +29,21 @@ storiesOf("Plugins", module)
       blockTypes={[BLOCK_CONTROL.UNORDERED_LIST_ITEM]}
       entityTypes={[ENTITY_CONTROL.LINK]}
       plugins={[linkify]}
+    />
+  ))
+  .add("Actions", () => (
+    <EditorWrapper
+      id="actions"
+      inlineStyles={[INLINE_CONTROL.BOLD]}
+      blockTypes={[
+        BLOCK_CONTROL.UNORDERED_LIST_ITEM,
+        {
+          type: "action-list-item",
+          label: "✔",
+          description: "Action list",
+        },
+      ]}
+      entityTypes={[ENTITY_CONTROL.LINK]}
+      plugins={[linkify, actionBlock]}
     />
   ));
