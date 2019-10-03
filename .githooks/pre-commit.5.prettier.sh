ef00c73a89c6fbc0a025941d89cddd3a0e4843a7
Thibaud Colas <thibaudcolas@gmail.com>
2019-10-04 00:29:49 +0100

    Clean up git hooks for formatting

--- .githooks/pre-commit.5.prettier.sh
+++ .githooks/pre-commit.5.prettier.sh
@@ -1,79 +1,13 @@
 #!/usr/bin/env bash
+
 # Format and re-stage fully staged files only.
-
-if [ -n "$JS_FULLY_STAGED" ];
+if [ -n "$PRETTIER_FULLY_STAGED" ];
 then
-  npx prettier --write $JS_FULLY_STAGED
-  git add $JS_FULLY_STAGED
+  npx prettier --write $PRETTIER_FULLY_STAGED
+  git add $PRETTIER_FULLY_STAGED
 fi
 
-if [ -n "$JS_STAGED" ];
+if [ -n "$PRETTIER_STAGED" ];
 then
-  npx prettier --check $JS_STAGED
-fi
-
-if [ -n "$SCSS_FULLY_STAGED" ];
-then
-  npx prettier --write $SCSS_FULLY_STAGED
-  git add $SCSS_FULLY_STAGED
-fi
-
-if [ -n "$SCSS_STAGED" ];
-then
-  npx prettier --check $SCSS_STAGED
-fi
-
-if [ -n "$CSS_FULLY_STAGED" ];
-then
-  npx prettier --write $CSS_FULLY_STAGED
-  git add $CSS_FULLY_STAGED
-fi
-
-if [ -n "$CSS_STAGED" ];
-then
-  npx prettier --check $CSS_STAGED
-fi
-
-if [ -n "$MD_FULLY_STAGED" ];
-then
-  npx prettier --write $MD_FULLY_STAGED
-  git add $MD_FULLY_STAGED
-fi
-
-if [ -n "$MD_STAGED" ];
-then
-  npx prettier --check $MD_STAGED
-fi
-
-if [ -n "$JSON_FULLY_STAGED" ];
-then
-  npx prettier --write $JSON_FULLY_STAGED
-  git add $JSON_FULLY_STAGED
-fi
-
-if [ -n "$JSON_STAGED" ];
-then
-  npx prettier --check $JSON_STAGED
-fi
-
-if [ -n "$YAML_FULLY_STAGED" ];
-then
-  npx prettier --write $YAML_FULLY_STAGED
-  git add $YAML_FULLY_STAGED
-fi
-
-if [ -n "$YAML_STAGED" ];
-then
-  npx prettier --check $YAML_STAGED
-fi
-
-if [ -n "$HTML_FULLY_STAGED" ];
-then
-  npx prettier --write $HTML_FULLY_STAGED
-  git add $HTML_FULLY_STAGED
-fi
-
-if [ -n "$HTML_STAGED" ];
-then
-  npx prettier --check $HTML_STAGED
+  npx prettier --check $PRETTIER_STAGED
 fi
