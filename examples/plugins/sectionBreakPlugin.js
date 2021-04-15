48119d3bd9f33146ae9746e4a0f1ed84ef89b7a3
jacobtoppm <jacobtm@torchbox.com>
2021-04-15 14:56:13 +0100

    Add shortcut to section-break plugin example

--- examples/plugins/sectionBreakPlugin.js
+++ examples/plugins/sectionBreakPlugin.js
@@ -1,12 +1,23 @@
 // @flow
 import React from "react";
 import type { Component, Node } from "react";
-import { ContentBlock, EditorState, Modifier } from "draft-js";
+import { ContentBlock, EditorState, KeyBindingUtil, Modifier } from "draft-js";
 
 import { ToolbarButton } from "../../lib";
 
+type PluginFunctions = {
+  setEditorState: (EditorState) => void,
+};
+
 const BREAK_ICON =
   "M0 16h4v2h-4zM6 16h6v2h-6zM14 16h4v2h-4zM20 16h6v2h-6zM28 16h4v2h-4zM27.5 0l0.5 14h-24l0.5-14h1l0.5 12h20l0.5-12zM4.5 32l-0.5-12h24l-0.5 12h-1l-0.5-10h-20l-0.5 10zM0 512h128v64H0v-64zm192 0h192v64H192v-64zm256 0h128v64H448v-64zm192 0h192v64H640v-64zm256 0h128v64H896v-64zM880 0l16 448H128L144 0h32l16 384h640L848 0h32zM144 1024l-16-384h768l-16 384h-32l-16-320H192l-16 320h-32z";
+
+const { isOptionKeyCommand } = KeyBindingUtil;
+// Copied from behavior.js.
+// Hack relying on the internals of Draft.js.
+// See https://github.com/facebook/draft-js/pull/869
+// $FlowFixMe
+const IS_MAC_OS = isOptionKeyCommand({ altKey: "test" }) === "test";
 
 const insertSectionBreak = (editorState: EditorState) => {
   const content = editorState.getCurrentContent();
@@ -35,7 +46,7 @@ export const SectionBreakControl = ({ getEditorState, onChange }: Props) => (
   <ToolbarButton
     name="SECTION_BREAK_CONTROL"
     icon={BREAK_ICON}
-    title="Section break"
+    title={`Section break\n${IS_MAC_OS ? "⌘ + Alt + S" : "Ctrl + Alt + S"}`}
     onClick={() => {
       onChange(insertSectionBreak(getEditorState()));
     }}
@@ -69,6 +80,23 @@ const sectionBreakPlugin = (config: {|
       }
 
       return null;
+    },
+    keyBindingFn(e: SyntheticKeyboardEvent<>) {
+      if ((e.metaKey || e.ctrlKey) && e.altKey && e.key.toLowerCase() === "s") {
+        return "break";
+      }
+      return undefined;
+    },
+    handleKeyCommand(
+      command: string,
+      editorState: EditorState,
+      { setEditorState }: PluginFunctions,
+    ) {
+      if (command === "break") {
+        setEditorState(insertSectionBreak(editorState));
+        return "handled";
+      }
+      return "not-handled";
     },
   };
 };
