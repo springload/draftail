bc667b61cf56f4c230b49b0faad8b8f3bada49b8
David Johnston <djohnston@whispir.com>
2021-09-21 14:40:02 +1000

    Add save handler tests

--- tests/rtl.test.js
+++ tests/rtl.test.js
@@ -5,6 +5,7 @@ import {
   getByText,
   render,
   screen,
+  waitFor,
 } from "@testing-library/react";
 import { DraftailEditor } from "../lib";
 
@@ -85,5 +86,187 @@ describe("Draftail", () => {
     });
 
     getByText(textbox, "abchello");
+  });
+
+  it("It fires the saveHandler when the component mounts", async () => {
+    const saveHandler = jest.fn();
+
+    render(
+      <DraftailEditor
+        ariaDescribedBy="foo"
+        ariaLabel="aria label"
+        inlineStyles={[]}
+        spellCheck={false}
+        rawContentState={{
+          entityMap: {},
+          blocks: [
+            {
+              key: "aaa",
+              text: "abc",
+            },
+          ],
+        }}
+        blockTypes={[]}
+        onSave={saveHandler}
+        placeholder="placeholder"
+        className="classname"
+      />,
+    );
+
+    // 100ms is too low
+    await new Promise((res) => setTimeout(res, 500));
+
+    expect(saveHandler).toHaveBeenCalledTimes(1);
+  });
+
+  it("It fires the saveHandler a second time when text is entered (timeouts technique)", async () => {
+    const saveHandler = jest.fn();
+
+    render(
+      <DraftailEditor
+        ariaDescribedBy="foo"
+        ariaLabel="aria label"
+        inlineStyles={[]}
+        spellCheck={false}
+        rawContentState={{
+          entityMap: {},
+          blocks: [
+            {
+              key: "aaa",
+              text: "abc",
+            },
+          ],
+        }}
+        blockTypes={[]}
+        onSave={saveHandler}
+        placeholder="placeholder"
+        className="classname"
+      />,
+    );
+
+    // 100ms is too low
+    await new Promise((res) => setTimeout(res, 500));
+
+    expect(saveHandler).toHaveBeenCalledTimes(1);
+
+    const textarea = screen.getByRole("textbox", {
+      name: "aria label",
+    });
+
+    const event = createEvent.paste(textarea, {
+      clipboardData: {
+        types: ["text/plain"],
+        getData: () => "hello",
+      },
+    });
+
+    fireEvent(textarea, event);
+
+    const textbox = screen.getByRole("textbox", {
+      name: "aria label",
+    });
+
+    getByText(textbox, "abchello");
+
+    await new Promise((res) => setTimeout(res, 500));
+
+    expect(saveHandler).toHaveBeenCalledTimes(2);
+  });
+
+  it("It fires the saveHandler a second time when text is entered (waitfor technique)", async () => {
+    const saveHandler = jest.fn();
+
+    render(
+      <DraftailEditor
+        ariaDescribedBy="foo"
+        ariaLabel="aria label"
+        inlineStyles={[]}
+        spellCheck={false}
+        rawContentState={{
+          entityMap: {},
+          blocks: [
+            {
+              key: "aaa",
+              text: "abc",
+            },
+          ],
+        }}
+        blockTypes={[]}
+        onSave={saveHandler}
+        placeholder="placeholder"
+        className="classname"
+      />,
+    );
+
+    await waitFor(() => expect(saveHandler).toHaveBeenCalledTimes(1));
+
+    const textarea = screen.getByRole("textbox", {
+      name: "aria label",
+    });
+
+    const event = createEvent.paste(textarea, {
+      clipboardData: {
+        types: ["text/plain"],
+        getData: () => "hello",
+      },
+    });
+
+    fireEvent(textarea, event);
+
+    const textbox = screen.getByRole("textbox", {
+      name: "aria label",
+    });
+
+    getByText(textbox, "abchello");
+    await waitFor(() => expect(saveHandler).toHaveBeenCalledTimes(2));
+  });
+
+  it("If you don't wait for the original onMount saveHandler, then the two onSaves will be batched into one", async () => {
+    const saveHandler = jest.fn();
+
+    render(
+      <DraftailEditor
+        ariaDescribedBy="foo"
+        ariaLabel="aria label"
+        inlineStyles={[]}
+        spellCheck={false}
+        rawContentState={{
+          entityMap: {},
+          blocks: [
+            {
+              key: "aaa",
+              text: "abc",
+            },
+          ],
+        }}
+        blockTypes={[]}
+        onSave={saveHandler}
+        placeholder="placeholder"
+        className="classname"
+      />,
+    );
+
+    const textarea = screen.getByRole("textbox", {
+      name: "aria label",
+    });
+
+    const event = createEvent.paste(textarea, {
+      clipboardData: {
+        types: ["text/plain"],
+        getData: () => "hello",
+      },
+    });
+
+    fireEvent(textarea, event);
+
+    const textbox = screen.getByRole("textbox", {
+      name: "aria label",
+    });
+
+    getByText(textbox, "abchello");
+
+    await new Promise((res) => setTimeout(res, 500));
+
+    expect(saveHandler).toHaveBeenCalledTimes(1);
   });
 });
