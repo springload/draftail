b91cceaddaffa137e397bef29c376f03f988b155
Thibaud Colas <thibaudcolas@gmail.com>
2022-05-19 01:35:18 +0100

    Simplify examples

--- tests/rtl.test.js
+++ tests/rtl.test.js
@@ -9,8 +9,8 @@ import {
 } from "@testing-library/react";
 import { DraftailEditor } from "../lib";
 
-describe("Draftail", () => {
-  it("Is selectable by aria tags", () => {
+describe("DraftailEditor RTL", () => {
+  it("selectable by ARIA attributes", () => {
     const saveHandler = jest.fn();
 
     render(
@@ -43,7 +43,7 @@ describe("Draftail", () => {
     getByText(textbox, "abc");
   });
 
-  it("You can update the state of it using RTL", async () => {
+  it("updateable with RTL APIs", async () => {
     const saveHandler = jest.fn();
 
     render(
@@ -86,187 +86,5 @@ describe("Draftail", () => {
     });
 
     getByText(textbox, "abchello");
-  });
-
-  it("It fires the saveHandler when the component mounts", async () => {
-    const saveHandler = jest.fn();
-
-    render(
-      <DraftailEditor
-        ariaDescribedBy="foo"
-        ariaLabel="aria label"
-        inlineStyles={[]}
-        spellCheck={false}
-        rawContentState={{
-          entityMap: {},
-          blocks: [
-            {
-              key: "aaa",
-              text: "abc",
-            },
-          ],
-        }}
-        blockTypes={[]}
-        onSave={saveHandler}
-        placeholder="placeholder"
-        className="classname"
-      />,
-    );
-
-    // 100ms is too low
-    await new Promise((res) => setTimeout(res, 500));
-
-    expect(saveHandler).toHaveBeenCalledTimes(1);
-  });
-
-  it("It fires the saveHandler a second time when text is entered (timeouts technique)", async () => {
-    const saveHandler = jest.fn();
-
-    render(
-      <DraftailEditor
-        ariaDescribedBy="foo"
-        ariaLabel="aria label"
-        inlineStyles={[]}
-        spellCheck={false}
-        rawContentState={{
-          entityMap: {},
-          blocks: [
-            {
-              key: "aaa",
-              text: "abc",
-            },
-          ],
-        }}
-        blockTypes={[]}
-        onSave={saveHandler}
-        placeholder="placeholder"
-        className="classname"
-      />,
-    );
-
-    // 100ms is too low
-    await new Promise((res) => setTimeout(res, 500));
-
-    expect(saveHandler).toHaveBeenCalledTimes(1);
-
-    const textarea = screen.getByRole("textbox", {
-      name: "aria label",
-    });
-
-    const event = createEvent.paste(textarea, {
-      clipboardData: {
-        types: ["text/plain"],
-        getData: () => "hello",
-      },
-    });
-
-    fireEvent(textarea, event);
-
-    const textbox = screen.getByRole("textbox", {
-      name: "aria label",
-    });
-
-    getByText(textbox, "abchello");
-
-    await new Promise((res) => setTimeout(res, 500));
-
-    expect(saveHandler).toHaveBeenCalledTimes(2);
-  });
-
-  it("It fires the saveHandler a second time when text is entered (waitfor technique)", async () => {
-    const saveHandler = jest.fn();
-
-    render(
-      <DraftailEditor
-        ariaDescribedBy="foo"
-        ariaLabel="aria label"
-        inlineStyles={[]}
-        spellCheck={false}
-        rawContentState={{
-          entityMap: {},
-          blocks: [
-            {
-              key: "aaa",
-              text: "abc",
-            },
-          ],
-        }}
-        blockTypes={[]}
-        onSave={saveHandler}
-        placeholder="placeholder"
-        className="classname"
-      />,
-    );
-
-    await waitFor(() => expect(saveHandler).toHaveBeenCalledTimes(1));
-
-    const textarea = screen.getByRole("textbox", {
-      name: "aria label",
-    });
-
-    const event = createEvent.paste(textarea, {
-      clipboardData: {
-        types: ["text/plain"],
-        getData: () => "hello",
-      },
-    });
-
-    fireEvent(textarea, event);
-
-    const textbox = screen.getByRole("textbox", {
-      name: "aria label",
-    });
-
-    getByText(textbox, "abchello");
-    await waitFor(() => expect(saveHandler).toHaveBeenCalledTimes(2));
-  });
-
-  it("If you don't wait for the original onMount saveHandler, then the two onSaves will be batched into one", async () => {
-    const saveHandler = jest.fn();
-
-    render(
-      <DraftailEditor
-        ariaDescribedBy="foo"
-        ariaLabel="aria label"
-        inlineStyles={[]}
-        spellCheck={false}
-        rawContentState={{
-          entityMap: {},
-          blocks: [
-            {
-              key: "aaa",
-              text: "abc",
-            },
-          ],
-        }}
-        blockTypes={[]}
-        onSave={saveHandler}
-        placeholder="placeholder"
-        className="classname"
-      />,
-    );
-
-    const textarea = screen.getByRole("textbox", {
-      name: "aria label",
-    });
-
-    const event = createEvent.paste(textarea, {
-      clipboardData: {
-        types: ["text/plain"],
-        getData: () => "hello",
-      },
-    });
-
-    fireEvent(textarea, event);
-
-    const textbox = screen.getByRole("textbox", {
-      name: "aria label",
-    });
-
-    getByText(textbox, "abchello");
-
-    await new Promise((res) => setTimeout(res, 500));
-
-    expect(saveHandler).toHaveBeenCalledTimes(1);
   });
 });
