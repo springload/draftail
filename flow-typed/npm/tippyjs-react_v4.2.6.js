d3b8b3779c62926009d1f13390f878a96361c6e0
Thibaud Colas <thibaudcolas@gmail.com>
2022-05-30 10:43:45 +0100

    Update Flow config

---
+++ flow-typed/npm/tippyjs-react_v4.2.6.js
@@ -0,0 +1,18 @@
+// @flow
+
+declare module "@tippyjs/react" {
+  declare type DefaultProps = {
+    visible?: boolean,
+    getReferenceClientRect?: () => {},
+    maxWidth?: string,
+    interactive?: boolean,
+    appendTo?: () => HTMLElement | null,
+    content?: React$Node,
+  };
+
+  declare type Props = DefaultProps & {};
+
+  declare class Tippy extends React$Component<Props> {}
+
+  declare module.exports: typeof Tippy;
+}
