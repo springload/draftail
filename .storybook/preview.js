529f7f933fb0d8d5ba40289700de6e12fd1af023
Thibaud Colas <thibaudcolas@gmail.com>
2022-06-01 16:34:12 +0100

    Filter React warnings in Storybook logs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,3 +14,18 @@ if (SENTRY_DSN) {
     release: PKG_VERSION,
   });
 }
+
+const consoleWarn = console.warn;
+
+console.warn = function filterWarnings(msg, ...args) {
+  // Stop logging React warnings we shouldn’t be doing anything about at this time.
+  const supressedWarnings = [
+    "Warning: componentWillMount",
+    "Warning: componentWillReceiveProps",
+    "Warning: componentWillUpdate",
+  ];
+
+  if (!supressedWarnings.some((entry) => msg.includes(entry))) {
+    consoleWarn.apply(console, ...args);
+  }
+};
